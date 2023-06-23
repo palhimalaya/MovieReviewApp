caa35f157057d3966ebd8e08030eec511169fdbc
palhimalaya <himalayapal123@gmail.com>
2023-06-23 16:26:47 +0545

    Install Devise Gem with JWT for Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  Rails.application.routes.draw do
+      devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+  end
   # Defines the root path route ("/")
   root "main#index"
 end
--- db/migrate/20230622101232_create_users.rb
+++ db/migrate/20230622101232_create_users.rb
@@ -2,9 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :password
-      t.string :role
+      t.string :role, null: false, default: "Audience"
 
       t.timestamps
     end
