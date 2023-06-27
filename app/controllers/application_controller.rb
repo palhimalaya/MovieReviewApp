f0daf4456453065845e855f77401e3e08c8ce39c
palhimalaya <himalayapal123@gmail.com>
2023-06-27 14:13:02 +0545

    Admin able to login to the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   respond_to :html, :json
+
+  def index
+    # Set the current_user variable
+    @current_user = current_api_v1_users_user
+  end
 end
