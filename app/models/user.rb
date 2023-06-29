fa4f5976bf763ef5f8c1726e08fd01b50f0d4488
cosine <cosine@pop-os.localdomain>
2023-06-29 11:31:52 +0545

    Validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,19 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :role, presence: true
+
+  validates :password, presence: true,
+                       length: { minimum: 8 },
+                       format: {
+                         with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}\z/,
+                         message: 'must include at least one lowercase letter, one uppercase letter, one symbol and one numeric digit'
+                       }
 end
