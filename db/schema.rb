e77cea2754f9a97e4c156f65286304614039aa2e
palhimalaya <himalayapal123@gmail.com>
2023-07-02 11:11:41 +0545

    Changed default role to audience

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_092057) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_02_051830) do
   create_table "movies", force: :cascade do |t|
     t.string "title", null: false
     t.string "description"
