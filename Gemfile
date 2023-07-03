f97b125165db7ee616054dc2ceb594ced8ba4c82
palhimalaya <himalayapal123@gmail.com>
2023-07-03 12:26:14 +0545

    use postgress

--- Gemfile
+++ Gemfile
@@ -96,3 +96,5 @@ gem 'active_model_serializers', '~> 0.10.13'
 gem 'faker'
 
 gem 'securerandom'
+
+gem 'pg'
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_02_051830) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string "title", null: false
     t.string "description"
@@ -19,25 +22,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_02_051830) do
     t.string "cover_img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
     t.string "role", default: "audience", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "jti", null: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "jti", null: false
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
