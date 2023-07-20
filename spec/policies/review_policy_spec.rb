db789fb63e04c0b824a6c2af42c3e346bf349b30
palhimalaya <himalayapal123@gmail.com>
2023-07-20 09:36:45 +0545

    Solve pr comments

--- spec/policies/review_policy_spec.rb
+++ spec/policies/review_policy_spec.rb
@@ -9,33 +9,29 @@ RSpec.describe(ReviewPolicy, type: :policy) do
   let(:admin_user) { build(:user) }
   let(:movie) { build(:movie) }
 
-  shared_examples 'grants access to any user' do
-    it 'grants access' do
-      expect(subject).to(permit(audience_user, movie))
-    end
+  shared_examples 'grants access to audience and admin users' do
+    it { expect(subject).to(permit(audience_user, movie)) }
   end
 
-  shared_examples 'denies access to any user' do
-    it 'denies access' do
-      expect(subject).not_to(permit(audience_user, movie))
-    end
+  shared_examples 'denies access to audience and admin users' do
+    it { expect(subject).not_to(permit(audience_user, movie)) }
   end
 
   permissions :index? do
-    include_examples 'grants access to any user'
+    include_examples 'grants access to audience and admin users'
   end
 
   permissions :create? do
-    context 'when the user is the Audience' do
+    context 'when the user is an audience user' do
       before { movie.user = audience_user }
 
-      it 'grants access' do
+      it 'allows the audience user to add review to a movie' do
         expect(subject).to(permit(audience_user, movie))
       end
     end
 
-    context 'when the user is Admin' do
-      it 'denies access' do
+    context 'when the user is an admin user' do
+      it 'denies the admin user to add review to a movie' do
         expect(subject).not_to(permit(admin_user, movie))
       end
     end
